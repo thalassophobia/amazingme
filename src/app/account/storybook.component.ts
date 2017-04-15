c6b37b90487df83dd30f425b65dbbd859b2457d6
Ph3nyclidine <p.claudepeon@gmail.com>
2017-04-15 15:44:08 -0400

    Reformatted lots of uglyness

--- src/app/account/storybook.component.ts
+++ src/app/account/storybook.component.ts
@@ -9,16 +9,13 @@ import { ButtonsModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { RatingModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { PaginationModule } from 'ng2-bootstrap';
 import { Page } from './page';
-// import { PageService } from './page.service';
 
 import { Milestone } from './milestone';
 import { MilestoneService } from './milestones.service';
-// import { CHECKLIST } from './checklist';
 
 import { AchReport } from './report';
 import { Report } from './report';
 
-// import { MdDialog, MdDialogRef, MdDialogConfig} from '@angular/material';
 import { DialogsService} from './dialog.service';
 import { achDialogsService} from './achDialog.service';
 
@@ -26,20 +23,18 @@ import { achDialogsService} from './achDialog.service';
 import {AngularFire, FirebaseObjectObservable,FirebaseListObservable,AuthProviders, AuthMethods} from 'angularfire2';
 import {Subject} from 'rxjs/Subject';
 // todo: change to ng2-bootstrap
-//import { ModalDirective } from '../../../node_modules/ng2-bootstrap/components/modal/modal.component';
  
 @Component({
-
   templateUrl: './storybook.html'
 })
 
 export class StorybookComponent implements OnInit {
 
-  @ViewChild('myAudio') el:ElementRef;
+  @ViewChild('myAudio') el: ElementRef;
 
 
-  public totalItems:number = 400;
-  public currentPage:number = 1;
+  public totalItems: number = 400;
+  public currentPage: number = 1;
 
   public page: any;
   public selectedPage: Page;
@@ -54,18 +49,15 @@ export class StorybookComponent implements OnInit {
 
   public userID: any;
   public userAccount: FirebaseListObservable<any[]>;
-  public key:any;
+  public key: any;
 
-  //public userList: FirebaseListObservable<any>;
-  
-  // public selectedMilestone: Milestone;
   public showMilestone: boolean = true;
  
-  public maxSize:number = 10;
-  public bigTotalItems:number = 310;
-  public bigCurrentPage:number = 1;
+  public maxSize: number = 10;
+  public bigTotalItems: number = 310;
+  public bigCurrentPage: number = 1;
  
-  public noteUpdate:boolean = false;
+  public noteUpdate: boolean = false;
 
   public tempTimeOnPage: Date;
   
@@ -74,14 +66,11 @@ export class StorybookComponent implements OnInit {
     public af: AngularFire,
 
     private router: Router,
-    // private http: Http,
-    // private pageService: PageService,
     private dialogsService: DialogsService,
     private milestoneService: MilestoneService,  
     private viewContainerRef: ViewContainerRef,
     private achDialogsService: achDialogsService,
     private rd: Renderer
-
     ) {
 
     af.auth.subscribe(auth => {
@@ -100,22 +89,15 @@ export class StorybookComponent implements OnInit {
       }
     });
 
-    this.pages.subscribe(queriedItems => {
-      // this.selectedPage = this.page[0]; 
-      
-      // this.page = this.pages.flatMap(list => list).first().map(({id, img, milestoneID}) => ({id, img, milestoneID}));
+    this.pages.subscribe(queriedItems => {      
       console.log(queriedItems);  
-      // console.log("selectedPage: ", this.pages.flatMap(list => list).first());
-      // this.onSelect(this.pages.flatMap(list => list).first());
+
       console.log("selectedPage: ", queriedItems[0]);
       this.page = queriedItems[0];
       this.onSelect(queriedItems[0]);
-
-      // console.log("this page is: ", this.page[0]);
     });
     
 /*checklist*/
-
     this.userAccount = af.database.list('/userList',{
       query: {
         orderByChild: 'userID',
@@ -130,24 +112,6 @@ export class StorybookComponent implements OnInit {
           orderByChild: 'progress',
         }
       });
-
-
-      // var templist1 = af.database.list('/userList/'+this.key+'/Checklist/', {
-      //   query: {
-      //     orderByChild: 'progress',
-      //     equalTo: 0
-      //   }
-      // });
-      // templist1.subscribe(queriedItems => {this.tempReport.numRecord = 34- queriedItems.length});
-
-      // var templist2 = af.database.list('/userList/'+this.key+'/Checklist/', {
-      //   query: {
-      //     orderByChild: 'progress',
-      //     equalTo: 100
-      //   }
-      // });
-      // templist2.subscribe(queriedItems => {this.tempReport.numAchieved = queriedItems.length});
-
 
       this.checklistSubject = new Subject();
       this.checklist = af.database.list('/userList/'+queriedItems[0].$key+'/Checklist/', {
@@ -178,94 +142,44 @@ export class StorybookComponent implements OnInit {
           }
         });
         templist2.subscribe(queriedItems => {this.tempReport.numAchieved = queriedItems.length});
-
       });
-
     });
   }
 
-    // var templist1 = af.database.list('/userList', {
-    //     query: {
-    //       orderByChild: 'progress',
-    //       // equalTo: 0
-    //     }
-    //   });
-    // templist1.subscribe(queriedItems => {this.tempReport.numRecord =  queriedItems.length});
-
-    // var templist2 = af.database.list('/userList/'+this.userID+'/Checklist', {
-    //   query: {
-    //     orderByChild: 'progress',
-    //     equalTo: 100
-    //   }
-    // });
-    // templist2.subscribe(queriedItems => {this.tempReport.numAchieved = queriedItems.length});
-
-    // this.userChecklist = af.database.list('/userList/'+this.userID+'/Checklist');
-
-
-
   ngOnInit(): void {
-
-        
-    // this.getPages();
-    
-    // this.getReport().then(AchReport => this.reportUpdate(AchReport)); 
-
-    // this.getMilestones();
     this.setPage(1);
-    // this.countUpdateIteration().then(tempReport => this.openAch(tempReport));
   }
 
 
   public setPage(pageNo:number):void {
     this.currentPage = pageNo;
-    // this.pageSubject.next(pageNo); 
      this.pageSubject.next(pageNo);
      console.log("setPage:",pageNo);
-    // console.log("setPage:",this.pageSubject.next(pageNo));
-    // this.pageService.getPage(pageNo).then(page => this.onSelect(page));
-    
   };
 
-  public pageChanged(event:any):void {
+  public pageChanged(event: any): void {
     console.log('Page changed to: ' + event.page);
     console.log('Number items per page: ' + event.itemsPerPage);
 
-    // this.pageService.getPage(event.page).then(page => this.onSelect(page));
-    // this.onSelect(event.page);
     this.pageSubject.next(event.page);
 
     let list2 = this.af.database.list('/userList/'+this.key+'/userLogs'+'/navigation');
     list2.push({ time: Date(), page: event.page});
-    // var x = document.getElementById("myAudio").;
-    // this.loadAudio();
-   
-
-
   };
   
 
   onSelect(page): void {
-    
-    // this.pageSubject.next(eventPage); 
-    // console.log("onselect:",this.page[0]);
     this.page = page;
 
-    if(page.milestoneID > 0){
+    if(page.milestoneID > 0) {
       this.checklistSubject.next(page.milestoneID);
-      // this.selectedMilestone = page.milestoneID;
       this.showMilestone = true;
-    }
-    else{
+    } else {
       this.showMilestone = false;
     }
-
-    
-
-    // this.milestoneService.getMilestone(this.selectedPage.milestoneID).then(milestone => this.selectedMilestone = milestone);
   }
 
-  public defineStar(milestone:any): void {
+  public defineStar(milestone: any): void {
     this.rate = milestone.progress/10;
     this.overStar = milestone.progress/10;
     this.percent = milestone.progress;
@@ -273,10 +187,7 @@ export class StorybookComponent implements OnInit {
   }
 
   /*dialog*/
-
-   public open(milestone:Milestone, noteUpdate:boolean) {
-
-
+   public open(milestone: Milestone, noteUpdate: boolean) {
     this.dialogsService
       .confirm(milestone, this.viewContainerRef)
       .subscribe(res => {
@@ -292,93 +203,61 @@ export class StorybookComponent implements OnInit {
           let list2 = this.af.database.list('/userList/'+this.key+'/userLogs'+'/noteUpdate');
           list2.push({ time: Date(), name: milestone.name, progress: milestone.progress, updatedNotes: milestone.notes,location: "story"});
         }
-        
-
-
       });
 
 
     let list = this.af.database.list('/userList/'+this.key+'/userLogs'+'/openDialog');
-    list.push({ name: milestone.name, time: Date(),location: "story"} );
+    list.push({ name: milestone.name, time: Date(), location: "story"} );
    
   }
 
-   public openAch(report:Report) {
-      
+   public openAch(report: Report) {
     this.achDialogsService
       .confirm(report, this.viewContainerRef)
       .subscribe(res => this.directPage(res));
   }
 
   public openGames() {
-    window.location.href='http://claudepeon.com/docs/';
+    if (this.page.milestoneID === 10) {
+      window.location.href='http://claudepeon.com/docs/ingredient_game_2.html';
+    } else if (this.page.milestoneID === 20) {
+      // window.location.href='http://claudepeon.com/docs/puzzle_game.html;
+    } else if (this.page.milestoneID === 15) {
+      // window.location.href='http://claudepeon.com/docs/shapes_game.html;
+    }
   }
 
-  public refreshPage(res:any) {
-    if (res == true){
-
-     // if(this.page.milestoneID > 0){
-     //  this.milestoneService.getMilestone(this.selectedPage.milestoneID)
-     //  .then(milestone => this.selectedMilestone = milestone)
-     //  .then(selectedMilestone => this.defineStar(selectedMilestone));
-     //  this.showMilestone = true;
-      
-     // }
-     // else{
-     //  this.showMilestone = false;
-     // }
-
-    if(this.page.milestoneID > 0){
+  public refreshPage(res: any) {
+    if (res == true && this.page.milestoneID > 0) {
       this.checklistSubject.next(this.page.milestoneID);
 
       console.log("refreshpage");
       this.showMilestone = true;
-      }
-    else{
+      } else{
       this.showMilestone = false;
       }
-
-
-    }
-
-    // this.getReport().then(AchReport => this.reportUpdate(AchReport)); 
-    //this.getReport().then(AchReport => this.tempReport = AchReport);//refresh the report if any change is made
   }
 
-
-  public directPage(res:any) {
-    if (res == true){
+  public directPage(res: any) {
+    if (res == true) {
       this.setPage(3);
-    }
-    else {
+    } else {
       this.setPage(1);
-
     }
-  
   }
 
-
-  // public openDialog(Number:number):void {
-  //   this.milestoneService.getMilestone(Number).then(milestone => this.open(milestone));
-  // };
-
- 
-  //public selectedMilestone: Milestone;
   public result: any;
 
-
-
   /*slider*/
-
-  public x:number = 5;
-  public y:number = 2;
-  public max:number = 10;
-  public rate:number = 0;
-  public isReadonly:boolean = false;
-  public overStar:number;
-  public percent:number;
+  public x: number = 5;
+  public y: number = 2;
+  public max: number = 10;
+  public rate: number = 0;
+  public isReadonly: boolean = false;
+  public overStar: number;
+  public percent: number;
  
-  public ratingStates:any = [
+  public ratingStates: any = [
     {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
     {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
     {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
@@ -386,19 +265,18 @@ export class StorybookComponent implements OnInit {
     {stateOff: 'glyphicon-off'}
   ];
  
-  public hoveringOver(value:number):void {
+  public hoveringOver(value: number): void {
     this.overStar = value;
     this.percent = 10 * value ;
     console.log("hoveringOver:",value);
   };
  
-  public resetStar():void {
+  public resetStar(): void {
     this.overStar = 0;
   }
 
-  public clickOn(value:number):void {
+  public clickOn(value: number): void {
     this.overStar = value;
-    //this.rate = value;
     this.percent = 10 * value;
 
     /*Update Progress*/
@@ -406,54 +284,13 @@ export class StorybookComponent implements OnInit {
     /*Log Progress*/
     let list = this.af.database.list('/userList/'+this.key+'/userLogs'+'/recordProgress');
     list.push({ time: Date(), name: this.selectedMilestone.name, progress: this.percent });
-
-    
   };
 
-
-
   /*record update*/
-
-
   public checklistLength: number = 34;
-
   public tempReport: Report = AchReport;
 
   public getReport(): Promise<Report> {
-    
     return Promise.resolve(AchReport);
   }
-
-
-  // @HostListener('mouseenter') onMouseEnter() {
-  //   this.highlight('yellow');
-  // }
-  // @HostListener('mouseleave') onMouseLeave() {
-  //   this.highlight(null);
-  // }
-  // private audioLoad() {
-  //   this.el.nativeElement.load();
-  // }
-
-  // ngAfterViewInit() {
-  //    this.rd.invokeElementMethod(this.el.nativeElement,'load');
-  // }
- 
-  
-
-  
-
-
 }
-
-
-// @Directive({
-//   selector: '[myAudio]'
-// })
-// export class AudioDirective {
-//   constructor(private el: ElementRef) { }
-  
-//   private audioLoad() {
-//     this.el.nativeElement.load();
-//   }
-// }
